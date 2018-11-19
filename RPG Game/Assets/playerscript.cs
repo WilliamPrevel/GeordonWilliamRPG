b4fb15ab5bed18dd1b26a1376a06bad2dcee0943
Neoteuthis <Neoteuthis@users.noreply.github.com>
2018-11-19 15:44:01 -0500

    updated character

---
+++ RPG Game/Assets/playerscript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerscript : MonoBehaviour {
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public Animation walk;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        // let the gameObject fall down
+        gameObject.transform.position = new Vector3(0, 5, 0);
+        walk = GetComponent<Animation>();
+    foreach (AnimationState state in walk)
+    {
+        state.speed = 0.5F;
+    }
+    }
+
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            // We are grounded, so recalculate
+            // move direction directly from axes
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection = moveDirection * speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        // Apply gravity
+        moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+
+        // Move the controller
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
