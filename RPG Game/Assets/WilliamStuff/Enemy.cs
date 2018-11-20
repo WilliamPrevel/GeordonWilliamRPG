46d1da082b3fd5a15301d42ec1f0346fd8662c70
William <wprevel9@gmail.com>
2018-11-20 14:30:45 -0500

    Made enemy and item test scripts

---
+++ RPG Game/Assets/WilliamStuff/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public Transform player;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        if (Vector3.Distance(player.position, this.transform.position) < 10)
+            
+        {
+
+            Vector3 direction = player.position - this.transform.position;
+            
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
+
+            Quaternion.LookRotation(direction), 0.1f); 
+
+            if (direction.magnitude > 5)
+            {
+
+                this.transform.Translate(0, 0, 0.05f); 
+
+            }
+
+        }
+
+    }
+
+}
---
+++ RPG Game/Assets/WilliamStuff/ItemPickUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    private void OnTriggerEnter(Collider player)
+    {
+        if(player.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
