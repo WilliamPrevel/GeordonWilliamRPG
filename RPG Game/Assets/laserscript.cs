86a38660ba2ce23466ca6c04f688e64aa5f7365d
Neoteuthis <Neoteuthis@users.noreply.github.com>
2018-11-26 16:19:57 -0500

    started on laser beam

---
+++ RPG Game/Assets/laserscript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laserscript : MonoBehaviour {
+    public LineRenderer laser;
+    GameObject hitplayer;
+    RaycastHit hit;
+    public int range;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, range))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.green);
+            if (hit.transform.gameObject.tag == "Player")
+            {
+                hitplayer = hit.transform.gameObject;
+                hit.transform.gameObject.GetComponent<playerscript>();
+                hitplayer.GetComponent<playerscript>().HP -= 1;
+                // Debug.Log("Hit Player");
+            }
+        }
+        for (int i = 0; i < range; i++)
+        {
+            laser.SetPosition(i,hit.transform.position);
+        }
+    }
+}
