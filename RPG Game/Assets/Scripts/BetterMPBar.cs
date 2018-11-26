ba7af3a65fccf12011d7f9e352afc1dcccf7d1b4
Neoteuthis <Neoteuthis@users.noreply.github.com>
2018-11-26 11:14:49 -0500

    added exp bar to UI
    I don't know what font you used, so could you make the EXP name so they match?

--- RPG Game/Assets/Scripts/BetterMPBar.cs
+++ RPG Game/Assets/Scripts/BetterMPBar.cs
@@ -7,7 +7,11 @@ public class BetterMPBar : MonoBehaviour {
 
     float MP;
     float MAXMP;
+    float EXP;
+    float MAXEXP;
     float currentmp;
+    float currentexp;
+    public bool isEXPbar = false;
     public GameObject player;
     // Use this for initialization
     void Start()
@@ -18,10 +22,18 @@ public class BetterMPBar : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        if (isEXPbar)
+        {
+        EXP = player.GetComponentInParent<playerscript>().exp;
+        MAXEXP = player.GetComponentInParent<playerscript>().LV*100;
+        currentexp = EXP / MAXEXP;
+        gameObject.transform.localScale = new Vector3(currentexp, 1, 1);
+        } else
+        {
         MP = player.GetComponentInParent<playerscript>().MP;
         MAXMP = player.GetComponentInParent<playerscript>().MAXMP;
         currentmp = MP / MAXMP;
         gameObject.transform.localScale = new Vector3(currentmp, 1, 1);
-       // Debug.Log(currentmp);
+        }// Debug.Log(currentmp);
     }
 }
