e6ab8645545bb67bd5222200f0a8cb6270d46871
William <wprevel9@gmail.com>
2018-11-26 06:54:42 -0500

    Spawner and Pool Created

---
+++ RPG Game/Assets/Scripts/Enemy Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject AnEnemy;
+    public MultiPurposePool thePool;
+
+    void Start()
+    {
+        StartCoroutine(SpawnTimer());
+    }
+
+    IEnumerator SpawnTimer()
+    {
+        while (true)
+        {
+            Spawn(AnEnemy);
+
+            yield return new WaitForSeconds(4f);
+        }
+    }
+
+    void Spawn(GameObject enemy)
+    {
+        float randomZ = Random.Range(-100f, 100f);
+        float randomX = Random.Range(-100f, 100f);
+
+        GameObject newEnemy = thePool.GetPooledObject(enemy.tag);
+        newEnemy.transform.position = new Vector3(randomX, 10, randomZ);
+        newEnemy.SetActive(true);
+    }
+}
---
+++ RPG Game/Assets/Scripts/Multi Purpose Pool.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiPurposePool : MonoBehaviour {
+
+    public List<GameObject> pooledObjects;
+    public GameObject objectToPoolA;
+    public int amountToPool;
+
+    public static MultiPurposePool SharedInstance;
+
+    void Awake()
+    {
+        SharedInstance = this;
+    }
+    // Use this for initialization
+    void Start()
+    {
+        pooledObjects = new List<GameObject>();
+        for (int i = 0; i < amountToPool; i++)
+        {
+            pooledObjects.Add(setGameObj(objectToPoolA));
+        }
+
+    }
+
+    GameObject setGameObj(GameObject objToPool)
+    {
+        GameObject obj = (GameObject)Instantiate(objToPool);
+        obj.SetActive(false);
+        return obj;
+    }
+
+    public GameObject GetPooledObject(string tag)
+    {
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+            if (pooledObjects[i] && !pooledObjects[i].activeInHierarchy && pooledObjects[i].tag == tag)
+            {
+                return pooledObjects[i];
+            }
+        }
+        GameObject blah = null;
+        if (tag == objectToPoolA.tag)
+        {
+            blah = objectToPoolA;
+        }
+        
+        
+
+        GameObject obj = (GameObject)Instantiate(blah);
+        obj.SetActive(false);
+        pooledObjects.Add(obj);
+        return obj;
+
+    }
+}
