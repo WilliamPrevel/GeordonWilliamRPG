f7b83753bd9d0a7b5a72c973764a75851d8d63cf
William <wprevel9@gmail.com>
2018-11-29 02:36:27 -0500

    some stuff added
    Yes

---
+++ RPG Game/Assets/Scripts/LevelNumber.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelNumber : MonoBehaviour {
+
+    private int level;
+    Text LevelText;
+    public GameObject Player;
+    
+    
+
+    // Use this for initialization
+    void Start () {
+        LevelText = gameObject.GetComponent<Text>();
+        level = Player.GetComponentInParent<playerscript>().LV;
+        LevelText.text = "" + level;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        LevelText.text = "" + level;
+        
+	}
+}
