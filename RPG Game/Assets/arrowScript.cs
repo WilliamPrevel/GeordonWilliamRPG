43fcbfd4d439436131cb4aff766e4ab2f6f3ad67
Neoteuthis <Neoteuthis@users.noreply.github.com>
2018-12-04 12:52:22 -0500

    added arrows

---
+++ RPG Game/Assets/arrowScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrowScript : MonoBehaviour
+{
+    public float movespeed = 0.2f;
+    public float killtime = 100;
+    public int power = 10;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Translate(Vector3.forward * movespeed);
+        killtime--;
+        if (killtime <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        movespeed = 0;
+        //if (collision.gameObject.tag == "Enemy")
+        if(collision.gameObject.GetComponent<Enemy>() != null)
+        {
+            collision.gameObject.GetComponent<Enemy>().myStats.Health -= power;
+        }
+    }
+}
