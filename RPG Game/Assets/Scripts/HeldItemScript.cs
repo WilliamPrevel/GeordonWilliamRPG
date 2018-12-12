fb1a16905545da519b23fe70be6df263f039eefa
Neoteuthis <Neoteuthis@users.noreply.github.com>
2018-12-12 14:43:42 -0500

    fixed again

--- RPG Game/Assets/Scripts/HeldItemScript.cs
+++ RPG Game/Assets/Scripts/HeldItemScript.cs
@@ -6,6 +6,8 @@ public class HeldItemScript : MonoBehaviour {
     public string MyName = "Spork";
     public GameObject thePlayer;
     public bool isDefault = false;
+    public bool isWeapon = true;
+    public bool isArmour = false;
 	// Use this for initialization
 	void Start () {
         thePlayer = GameObject.FindWithTag("Player");
@@ -14,39 +16,46 @@ public class HeldItemScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         //for weapons
-        if (isDefault)
+        if (isWeapon)
         {
-            if (GameManager.Inventory.Count == 0)
+            if (isDefault)
             {
-                this.transform.localScale = new Vector3(1, 1, 1);
-            }
-            else
-            {
-                this.transform.localScale = new Vector3(0, 0, 0);
-            }
-        } else
-        {
-            if (GameManager.Inventory.Count > 0)
-            {
-                if (thePlayer.GetComponent<PlayerScript>().currentWeapon.GetComponent<Weapon>().myStats.weaponName == MyName)
+                if (GameManager.Inventory.Count == 0)
                 {
-                    this.transform.localScale = new Vector3(0.273f , 0.273f, 0.273f);
+                    this.transform.localScale = new Vector3(1, 1, 1);
                 }
                 else
                 {
                     this.transform.localScale = new Vector3(0, 0, 0);
                 }
-            } else
+            }
+            else
+            {
+                if (GameManager.Inventory.Count > 0)
                 {
-                    this.transform.localScale = new Vector3(0,0,0);
+                    if (thePlayer.GetComponent<PlayerScript>().currentWeapon.GetComponent<Weapon>().myStats.weaponName == MyName)
+                    {
+                        this.transform.localScale = new Vector3(0.273f, 0.273f, 0.273f);
+                    }
+                    else
+                    {
+                        this.transform.localScale = new Vector3(0, 0, 0);
+                    }
                 }
+                else
+                {
+                    this.transform.localScale = new Vector3(0, 0, 0);
+                }
+            }
         }
         //use this script for armor
+        if (isArmour)
+        {
             if (GameManager.Inventory2.Count > 0)
             {
                 if (thePlayer.GetComponent<PlayerScript>().currentArmour.GetComponent<Armour>().myStats.armourName == MyName)
                 {
-                    this.transform.localScale = new Vector3(0.273f, 0.273f, 0.273f);
+                    this.transform.localScale = new Vector3(0.07f, 0.07f, 0.07f);
                 }
                 else
                 {
@@ -57,6 +66,6 @@ public class HeldItemScript : MonoBehaviour {
             {
                 this.transform.localScale = new Vector3(0, 0, 0);
             }
-        
-	}
+        }
+    }
 }
