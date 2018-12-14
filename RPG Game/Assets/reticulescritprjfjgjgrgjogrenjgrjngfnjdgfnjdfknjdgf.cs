ea910d31b07879e9ecc8b65f76e6bd1600a76332
Neoteuthis <Neoteuthis@users.noreply.github.com>
2018-12-14 14:48:36 -0500

    I don't know why the hp hat keeps modding itself...

---
+++ RPG Game/Assets/reticulescritprjfjgjgrgjogrenjgrjngfnjdgfnjdfknjdgf.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reticulescritprjfjgjgrgjogrenjgrjngfnjdgfnjdfknjdgf : MonoBehaviour {
+    GameObject myCanvas;
+    GameManager theBoss;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 pos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(myCanvas.transform as RectTransform, Input.mousePosition, Camera.main, out pos);
+        transform.position = myCanvas.transform.TransformPoint(pos);
+    }
+}
