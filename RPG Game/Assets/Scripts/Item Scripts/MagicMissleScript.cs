9ae15ca3f5ed0a3dc7415a08d4e27f79ca7b8b66
William <wprevel9@gmail.com>
2018-12-18 16:30:12 -0500

    stuff

---
+++ RPG Game/Assets/Scripts/Item Scripts/MagicMissleScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicMissleScript : arrowScript {
+
+	// Use this for initialization
+	void Start () {
+        movespeed = 0.1f;
+        killtime = 30;
+        power = 30;
+}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
