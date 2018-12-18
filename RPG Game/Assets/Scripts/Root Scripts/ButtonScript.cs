a144b60515db576aac55968064d4f33649093ac0
William <wprevel9@gmail.com>
2018-12-18 15:50:01 -0500

    stuffs

---
+++ RPG Game/Assets/Scripts/Root Scripts/ButtonScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour {
+    
+    
+	public void ChangeScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+    
+}
