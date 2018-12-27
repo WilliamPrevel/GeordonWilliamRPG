2b038b9f6a4469c01e9c01c243219cd74b06ef2d
Neoteuthis <Neoteuthis@users.noreply.github.com>
2018-12-27 14:33:21 -0500

    added to UI
    in samplae scene. not a prefab yet.

--- RPG Game/Assets/Scripts/Camera Scripts/cameraScript.cs
+++ RPG Game/Assets/Scripts/Camera Scripts/cameraScript.cs
@@ -6,11 +6,11 @@ public class cameraScript : MonoBehaviour
 public float HSpeed = 4.0F;
 public float VSpeed = -4.0F;
     public GameObject player;
-    private Vector3 offset;
+   // private Vector3 offset;
 
     void Start()
     {
-        offset = transform.position - player.transform.position;
+       // offset = transform.position - player.transform.position;
     }
 
     void LateUpdate()
--- RPG Game/Assets/Scripts/dialogueBoxScript.cs
+++ RPG Game/Assets/Scripts/dialogueBoxScript.cs
@@ -3,26 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class dialogueBoxScript : MonoBehaviour {
+public class dialogueBoxScript : MonoBehaviour
+{
     public GameObject dialoguer;
+    public GameObject dialoguerMini;
+    public GameObject Player;
     public Text dialogueText;
+    public Text dialogueTextMini;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         //dialoguer = gameObject;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (GameManager.CurrentState == GameManager.GameState.Dialogue)
         {
             dialoguer.SetActive(true);
-        } else
+        }
+        else
         {
             dialoguer.SetActive(false);
         }
         //this._alpha = 100;
         //remove from update later, for now lets get it working.
         dialogueText.text = GameManager.CurrentMessage;
-	}
-    
+        ///
+        if(Player.GetComponent<PlayerScript>().currentWeapon.GetComponent<Weapon>().myStats.weaponName != null)
+        dialogueTextMini.text = Player.GetComponent<PlayerScript>().currentWeapon.GetComponent<Weapon>().myStats.weaponName;
+    }
 }
+    
